07026326017761fbd0419446467593d6c5ffe597
ksagep <sandor.kisgyorgy@gmail.com>
2022-05-29 20:01:49 +0000

    Finalize the script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
@@ -6,6 +7,10 @@ const answerButtonsElement = document.getElementById('answer-buttons');
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
     console.log('Started');
@@ -16,18 +21,60 @@ function startGame() {
     setNextQuestion();
 }
 
- 
-
 function setNextQuestion() {
+    resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+      const button = document.createElement('button');
+      button.innerText = answer.text;
+      button.classList.add('btn');
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener('click', selectAnswer);
+      answerButtonsElement.appendChild(button);
+    });
 }
 
-function selectAnswer() {
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstElementChild);
+  }
+}
 
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataser.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex +1) {
+    nextButton.classList.remove('hide'); 
+  }  else {
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.ClassList.add('correct');
+  } else {
+    element.ClassList.add('wrong');
+  }
+}
+
+function clearStatusClass(element) {
+    element.ClassList.remove('correct');
+    element.ClassList.remove('wrong');
 }
 
 const questions = [
@@ -41,16 +88,16 @@ const questions = [
         ]
     },
     {
-        question: "Which is not a roasting method?",
+        question: 'Which is not a roasting method?',
         answer:[
-          { text: "Spanish", correct: false },
-          { text: "Italian", correct: false },
+          { text: 'Spanish', correct: false },
+          { text: 'Italian', correct: false },
           { text: "French", correct: false },
           { text: "British", correct: true }
         ]        
       },
       {
-        question: "What color is the ripe coffee berry?",
+        question: 'What color is the ripe coffee berry?',
         answer:[
           { text: "Yellow", correct: false },
           { text: "Green", correct: false },
@@ -121,4 +168,5 @@ const questions = [
           { text: 'Vietnam', correct: false }
         ]
       }
+
 ];
