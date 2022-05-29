36f635e82144fcd9825aa823aba79bedf18f5079
ksagep <sandor.kisgyorgy@gmail.com>
2022-05-29 13:31:28 +0000

    Add basic structure of questions and possibility of shuffling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-const startButton = document.getElementById('start-btn')
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+
+const shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener('click', startGame);
 
 function startGame() {
-
+    console.log('Started');
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
 }
 
 function setNextQuestion() {
@@ -11,3 +19,15 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: '',
+        answers: [
+            {text: '', correct: ''},
+            {text: '', correct: ''},
+            {text: '', correct: 'true'},
+            {text: '', correct: 'false'}
+        ]
+    }
+]
