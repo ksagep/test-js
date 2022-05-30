29c9e942a8a67be5847033aa734aaa3228589fb1
ksagep <sandor.kisgyorgy@gmail.com>
2022-05-30 19:26:33 +0000

    Add score counter to the app.

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,6 +76,36 @@ function clearStatusClass(element) {
     element.classList.remove('wrong');
 }
 
+function checkAnswer() {
+
+  let userAnswer = document.getElementById("answer-buttons").value;
+  let calculatedAnswer = selectAnswer(correct);
+  let correct = userAnswer === calculatedAnswer[correct];
+
+  if (correct) {
+      incrementScore();
+  } else {
+      incrementFalseAnswer();
+  }
+}
+
+/** Gets the correct answers from the DOM and increments it by 1 */
+
+function incrementScore() {
+
+  let newScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++newScore;
+
+}
+
+/** Gets the incorrect answers from the DOM and increments it by 1 */
+function incrementFalseAnswer() {
+
+  let newScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++newScore;
+  
+}
+
 const questions = [
     {
         question: 'Where does coffee come from?',
